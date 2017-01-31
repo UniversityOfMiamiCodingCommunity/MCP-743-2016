f6f1607608a44b9c4959fa0da4416737c5adddea
Ashley Beecham <abeecham@med.miami.edu>
2017-01-31 10:47:42 -0500

    Numbers Assignment Class 3

---
+++ abeecham-numbers.py
@@ -0,0 +1,24 @@
+### Ashley's equations for Assignment 1 are noted below:
+##################################################
+
+###The following equations convert degrees Celsius to degrees Kelvin and Farenheit.
+###################################################################################
+
+#Temperature in Celsius
+#######################
+C = 25.0
+
+#Convert Celsius to Kelvin
+##########################
+K = C + 273.15
+
+#Convert Celsius to Farenheit
+#############################
+F = (C * (9/5)) + 32.0
+
+#Print C, K, and F to screen
+############################
+message = "Equation 1 indicates that " + str(C) + " degrees Celsius is " + str(F) + " degrees Farenheit and " + str(K) + " degrees Kelvin"
+print message
+
+
