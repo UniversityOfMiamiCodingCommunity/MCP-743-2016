7c1c498c9c6d11ee5f7f933457204515cd0252ba
amcgrew <Anna@Annas-MacBook-Air.local>
2017-02-06 04:09:03 -0500

    class 4 assignments

--- anna-class4-assignment-2.py
+++ anna-class4-assignment-2.py
@@ -28,14 +28,29 @@ print(sequence)
 
 startCodon = 'ATG'
 
+file = open("startCodonIndices.txt", 'w')
+
 index = 0
-while index < len(weirdAssDna):
-	index = weirdAssDna.find(startCodon, index)
+count = 0
+while index < len(sequence):
+	index = sequence.find(startCodon, index)
 	if index == -1:
 		break
-	print('There was a start codon found at position', index)
+	count += 1
 	index += 1
+file.write('The total number of start codons is ')
+file.write(str(count))
+file.write('.\n')
 
+index = 0
+while index < len(sequence):
+	index = sequence.find(startCodon, index)
+	if index == -1:
+		break
+	file.write('There was a start codon found at position ')
+	file.write(str(index))
+	file.write('.\n')
+	index += 1
 
 # 3)
 # Find all possible stop codons in parsed DNA sequence.
@@ -44,23 +59,35 @@ while index < len(weirdAssDna):
 # a) the first line reports the total number of stopCodons
 # b) the additional lines report a single stop codon index 
 
+stopCodons = ["TAG", "TGA", "TAA"]
 
-# string = []
-# sequence = open('dataFiles/sequence-p53.fasta')
-# for line in sequence:
-# 	string.append(line.rstrip('\n'))
-# string = string[1:-1]
-# sequence = ', '.join(str(x) for x in string)
-# print(sequence)
-# sequence = ''
-# for line in string:
-# 	sequence += line
-# print(sequence)
+file2 = open("stopCodonIndices.txt", 'w')
 
-# 1) 
-# Open the subsequence of chromosome 17 containing the gene for p53, and parse the DNA sequence into a single string.
-# This sequence is in the file named sequence-p53.fasta in the dataFiles directory.
-#####################################################################################################################
+for stopCodon in stopCodons:
+	index = 0
+	count = 0
+	while index < len(sequence):
+		index = sequence.find(stopCodon, index)
+		if index == -1:
+			break
+		count += 1
+		index += 1
+file2.write('The total number of stop codons is ')
+file2.write(str(count))
+file2.write('.\n')
+
+index = 0
+for stopCodon in stopCodons:
+	while index < len(sequence):
+		index = sequence.find(stopCodon, index)
+		if index == -1:
+			break
+		file2.write('There was a ')
+		file2.write(stopCodon)
+		file2.write(' stop codon found at position ')
+		file2.write(str(index))
+		file2.write('.\n')
+		index += 1
 
 
 
