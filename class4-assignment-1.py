18f584bec65110f5ab607a52183de7d8ab348c11
dangerisom <disom@miami.edu>
2017-02-06 10:37:11 -0500

    Typo fix

--- class4-assignment-1.py
+++ class4-assignment-1.py
@@ -3,7 +3,7 @@
 # You have traveled to a new planet call Isom in the nearby Pharma solar system.
 # You pull out the next next next next generation sequencer in your pocket and begin sequencing alien organisms. 
 # Over the course of a day or two, you realize that life on this planet uses a radically different genetic code. 
-# This code consists of 26 unique nucleobase you label from A-to-Z using the human english alphabet.
+# This code consists of 26 unique nucleobases you label from A-to-Z using the human english alphabet.
 # You discover that, unlike life on earth, a codon on planet Isom consists of 6 nucleobases.
 # You discover that, as with life on earth, there is only one start codon: "AYGOGO".
 # You discover that, as with life on earth, there are three stop codons: "STOPME", "STOPIT", and "STOPGO"
