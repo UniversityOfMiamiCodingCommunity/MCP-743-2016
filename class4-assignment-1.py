8101c11069a2ad7fb5230461d9e7e9d0eabf2509
uzekonyte <ugne.zekonyte@med.miami.edu>
2017-02-06 10:53:20 -0500

    hw
    Not sure why files I haven’t touched want to commit changes..

---
+++ class4-assignment-1.py
@@ -0,0 +1,20 @@
+########################################################################################################################
+# Assignment
+# You have traveled to a new planet call Isom in the nearby Pharma solar system.
+# You pull out the next next next next generation sequencer in your pocket and begin sequencing alien organisms. 
+# Over the course of a day or two, you realize that life on this planet uses a radically different genetic code. 
+# This code consists of 26 unique nucleobase you label from A-to-Z using the human english alphabet.
+# You discover that, unlike life on earth, a codon on planet Isom consists of 6 nucleobases.
+# You discover that, as with life on earth, there is only one start codon: "AYGOGO".
+# You discover that, as with life on earth, there are three stop codons: "STOPME", "STOPIT", and "STOPGO"
+# You have your obtained your first sequence:
+########################################################################################################################
+weirdAssDna = " XUIOPDEHSUJQPQHDUMIEYDHNAPQLKGAYGOGODKSIGJIEHYSNOEKUJNPOQGTSFKSOJWNEIMSQYEBZXAUQGTBHSTOPGOBCDAFROKMYEVGSYJENUQTFARISHEMRXDCFWRSTOPMEYWTSGBSTOPITYTGEBSOIMNEY"
+
+# 1) Identify the index of each start codon in the DNA sequence. 
+# 2) Get the index of the first start codon.
+# 3) Identify the index of each stop codon in the DNA sequence. 
+# 4) Identify the stop codon, and it's index that is in frame with the first start codon encountered in the DNA sequence.
+# Note: you must use at least one "for" and "while" loop in your approach.
+# May the force be with you.
+#########################################################################################################################
