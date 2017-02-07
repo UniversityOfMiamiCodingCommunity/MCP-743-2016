18b834b65ceb5a297628e17ac19ca67f3f212cd4
Ashley Beecham <abeecham@med.miami.edu>
2017-02-07 15:51:31 -0500

    Ashley Assigment Class 2 - #1-2 complete

--- class4-assignment-2.py
+++ class4-assignment-2.py
@@ -3,6 +3,12 @@
 # This sequence is in the file named sequence-p53.fasta in the dataFiles directory.
 #####################################################################################################################
 
+fileInput = open("dataFiles/sequence-p53.fasta", "r")
+dna=""
+for line in fileInput:
+	dna +=line[0:-1]
+print dna
+
 # 2)
 # Find all possible start codons in parsed DNA sequence.
 # Write your results to file named startCodonIndices.txt.
@@ -10,6 +16,29 @@
 # a) the first line reports the total number of startCodons
 # b) the additional lines report a single start codon index
 #################################################################################################
+
+startCodon = "ATG"
+
+startCodonCount = 0
+i = 0
+start_index=[]
+fileOutput = open("startCodonIndices.txt", "w")
+
+while i < len(dna) - 2:
+	iCodon = dna[i:i+3]
+	if iCodon == startCodon:
+		start_index.append(i)
+		# Increment the start codon count.
+		#################################
+		startCodonCount += 1 
+	# Increment the index counter.
+	##############################
+	i += 1
+fileOutput.write("There are a total of " + str(startCodonCount) + " start codons." + '\n')
+for value in start_index:
+	fileOutput.write(str(value) + '\n')
+
+fileOutput.close()
 
 # 3)
 # Find all possible stop codons in parsed DNA sequence.
@@ -19,8 +48,28 @@
 # b) the additional lines report a single stop codon index 
 #################################################################################################
 
+stopCodons = ["TAG", "TGA", "TAA"]
 
+stopCodonCount = 0
+stop_index=[]
+fileOutput = open("stopCodonIndices.txt", "w")
 
+for stopCodon in stopCodons: 
+	i = 0
+	while i < len(dna) - 2:
+		iCodon = dna[i:i+3]
+		if iCodon == stopCodon:
+			stop_index.append(i)
+			# Increment the start codon count.
+		    #################################
+		    stopCodonCount += 1 
+		# Increment the index counter.
+		##############################
+		i += 1
+		
+fileOutput.write("There are a total of " + str(stopCodonCount) + " stop codons." + '\n')
+for value in stop_index:
+	fileOutput.write(str(value) + '\n')
 
-
+fileOutput.close()
 
