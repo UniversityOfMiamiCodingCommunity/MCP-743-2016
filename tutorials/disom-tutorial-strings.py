7f3c80f76149f84fec5c2f5f9f5ba68c58347274
dangerisom <disom@miami.edu>
2017-02-10 05:37:35 -0500

    Repo reorganization

--- tutorials/disom-tutorial-strings.py
+++ tutorials/disom-tutorial-strings.py
@@ -163,19 +163,3 @@ z2 = float(zCoordinateAsString2)
 from math import sqrt # This is how you import the sqrt function from the Python math library.
 distance = sqrt((x2-x1)**2 + (y2-y1)**2 + (z2-z1)**2) # This is the equation for the distance between two 3D-points.
 print("The distance between the two atoms is:", distance, "Angstroms")
-
-########################################################################################################################
-# Assignment
-# 1) Identify the index of each start codon in the DNA sequence provided below. 
-# 2) Get the index of the first start codon.
-# 3) Identify the index of each stop codon.
-# 4) Identify the stop codon, and it's index that is in frame with the first start codon encountered in the DNA sequence.
-# 5) Count the number of G bases. 
-# 6) Identify each index that corresponds to an A base.  
-# 7) Identify the length of the DNA sequence. 
-# 8) Split the DNA sequence into a list using the start codon. 
-# Note: all of the answers to this assignment are directly or indirectly provided in the code above.
-#########################################################################################################################
-startCodon = "ATG"
-stopCodons = ["TAG", "TGA", "TAA"]
-dna = "GGACGTTTAAAAGGGAAAAAATGGAACCACCCGGGATAATGAAATTTTATGGGCCCCACCAGGACTAAGATAGCGTGAATGTAAATAAATAGCCC"
