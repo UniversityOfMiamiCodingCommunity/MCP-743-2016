d6cf9f54f92c52388f610b7df14c5a366fe6d28c
sdelolio <sdelolio@med.miami.edu>
2017-02-15 09:45:22 -0500

    upload class 5 assignment dictionary

---
+++ assignments/Class_5_Assignment_Sam.py
@@ -0,0 +1,50 @@
+#Sam's Class 5 Assignment
+
+#Variables
+city = "phoenix"
+cityAsList = list(city)
+vowels = "aeiou"
+consonants = "bcdfghjklmnpqrstvwxyz"
+
+answerDict = {}
+
+answerDict[1] = "phoenix"
+answerDict[2] = "pho"
+answerDict[3] = "phoe"
+answerDict[4] = "o"
+answerDict[5] = "pho"
+answerDict[6] = "Who controls the past, controls the future. Who controls the present, controls the past."
+answerDict[7] = "Who controls the present, controls the past. Who controls the present, controls the past."
+answerDict[8] = "Who controls the present, controls the past. Who controls the past, controls the future."
+answerDict[9] = "Who controls the present, controls the past. Who controls the past, controls the future."
+answerDict[10] = "1"
+answerDict[11] = "1"
+answerDict[12] = "-1"
+answerDict[13] = "7"
+answerDict[14] = "i += 1 is inside the loop"
+answerDict[15] = "i = 0 is outside the loop"
+answerDict[16] = "condition"
+answerDict[17] = "7"
+answerDict[18] = "7"
+answerDict[19] = "14"
+answerDict[20] = "0"
+answerDict[21] = "3"
+answerDict[22] = "3"
+answerDict[23] = "3"
+answerDict[24] = "4"
+answerDict[25] = "4"
+answerDict[26] = "3"
+answerDict[27] = "i=7, j=3"
+answerDict[28] = "i=7, j=0"
+answerDict[29] = "i=14, j=0"
+answerDict[30] = "i=7, j=0"
+answerDict[31] = "i=7, j=0"
+answerDict[32] = "i=3, j=0"
+answerDict[33] = "i=7, j=7, k=49"
+answerDict[34] = "i=7, j=7, k=49, a=21"
+
+print(answerDict)
+
+
+
+
