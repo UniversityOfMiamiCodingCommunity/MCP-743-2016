a50e700baa2da3eb73ed0b5c12ccb755c2681d0e
sdelolio <sdelolio@med.miami.edu>
2017-02-15 09:42:06 -0500

    upload (current) class 4 assignment #2

--- assignments/class4-assignment-2.py
+++ assignments/class4-assignment-2.py
@@ -2,9 +2,26 @@
 # Open the subsequence of chromosome 17 containing the gene for p53, and parse the DNA sequence into a single string.
 # This sequence is in the file named sequence-p53.fasta in the dataFiles directory.
 #####################################################################################################################
+fileInput = open("/Users/SamuelDelOlio/GitHub/MCP-743/dataFiles/sequence-p53.fasta", "r")
+dna = ""
+for line in fileInput:
+	dna += line[0:-1]
+print(dna)
 
 # 2)
 # Find all possible start codons in parsed DNA sequence.
+
+startCodon = "ATG"
+i = 0
+while i < len(dna)-2:
+	iCodon = dna[i:i+3]
+	if iCodon == startCodon:
+		print("Start Codon Found at Index:", i)
+	i += 1
+
+#startCodonCount = dna.count(startCodon)
+#print(startCodonCount)	
+
 # Write your results to file named startCodonIndices.txt.
 # The file should be formatted such that:
 # a) the first line reports the total number of startCodons
