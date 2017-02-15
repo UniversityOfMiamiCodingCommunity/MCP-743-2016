856fac94f39386ee52a5730b811ea36783126db0
sdelolio <sdelolio@med.miami.edu>
2017-02-15 09:37:31 -0500

    upload class 3 assignment #2

--- assignments/class3-assignment-2.py
+++ assignments/class3-assignment-2.py
@@ -13,3 +13,54 @@
 startCodon = "ATG"
 stopCodons = ["TAG", "TGA", "TAA"]
 dna = "GGACGTTTAAAAGGGAAAAAATGGAACCACCCGGGATAATGAAATTTTATGGGCCCCACCAGGACTAAGATAGCGTGAATGTAAATAAATAGCCC"
+
+#Question 1 - Identify the index of each start codon in the DNA sequence 
+i = 0
+j = len(dna)-2
+while i < j:
+	icodon = dna[i:i+3]
+	if icodon == startCodon:
+		print("Found startCodon", startCodon, "at index", i)
+	i += 1
+
+#Question 2 - Get the index of the first start codon
+result1 = dna.find(startCodon)
+print("First startCodon appears at index",result1)
+
+#Question 3 - Identify the index of each stop codon
+for stopCodon in stopCodons:
+	i = 0
+	while i <len(dna)-2:
+		iCodon = dna[i:i+3]
+		if iCodon == stopCodon:
+			print("Found stopCodon", iCodon, "at index", i)
+		i += 1
+
+#Question 4 - Identify the stop codon, and it;s index that is in fram with the first start codon in the DNA sequence
+for stopCodon in stopCodons:
+	i = 0
+	while i < len(dna)-2:
+		sCodon = dna[i:i+3]
+		if sCodon == stopCodon:
+			if ((i + 3) - 20) % 3 == 0:
+				print(i)
+		i += 1
+			
+
+#Question 5 - Count the number of G bases
+G_count = dna.count("G")
+print("There are", G_count, "Guanines in the DNA sequence")
+
+#Question 6 - Identify each index that corresponds to an A base
+i = 0
+while i < len(dna):
+	A_base = dna[i:i+1]
+	if A_base == "A":
+		print("There is an Adenine at index", i)
+	i += 1
+
+#Question 7 - Identify the length of the DNA sequence
+print("The length of the DNA sequence is:", len(dna), "nucleotides")
+
+#Question 8 - Split the DNA sequence into a list using the start codon
+print(dna.split(startCodon))
