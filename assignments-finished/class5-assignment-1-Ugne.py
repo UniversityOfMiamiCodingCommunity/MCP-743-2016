2604add4fafa346e9f4bfa2489dc933c6229b75e
uzekonyte <ugne.zekonyte@med.miami.edu>
2017-02-20 10:12:52 -0500

    hw5 dictionary reformatted

---
+++ assignments-finished/class5-assignment-1-Ugne.py
@@ -0,0 +1,37 @@
+answerDict = {
+
+1: "sacramento",
+2: "sacra",
+3: "sacra",
+4: "a",
+5: "sacra",
+6: "Who controls the past, controls the future.\nWho controls the present, controls the past.",
+7: "Who controls the present, controls the past.\nWho controls the present, controls the past.",
+8: "Who controls the present, controls the past.\nWho controls the past, controls the future.",
+9: "Who controls the present, controls the past.\nWho controls the past, controls the future.",
+10: 1,
+11: 1,
+12: -1,
+13: 10,
+14: "i += 1",
+15: "i = 0",
+16: "conditional statement",
+17: 10,
+18: 10,
+19: 20,
+20: 0,
+21: 5,
+22: 4,
+23: 4,
+24: 6,
+25: 6,
+26: 4,
+27: (10, 4)
+28: (10, 0)
+29: (20, 0)
+30: (10, 0)
+31: (10, 0)
+32: (6, 0)
+33: (0, 10, 100)
+34: (20, 10, 100, 40)
+}
