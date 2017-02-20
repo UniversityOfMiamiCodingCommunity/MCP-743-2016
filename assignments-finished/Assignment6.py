c20d9fa67b787ae010c5ebc4be9c13e7e48c8676
nadiaxp <nxp379@med.miami.edu>
2017-02-20 10:41:47 -0500

    Assignment 6

---
+++ assignments-finished/Assignment6.py
@@ -0,0 +1,25 @@
+#Nadia Assignment 6
+import math
+math.pi
+# A = l * w
+# A = Area in inches squared of rectangle
+# l = lenght in inches
+# w = width in inches 
+def AreaofRectangle(l,w,A): #define function
+	#Solve for Area
+	A = l * w
+	print ("Area result from function:", A)
+	return A
+
+# C = 2 * r * pi
+# C = circumference of circle 
+# r = radius of circle
+import math
+print (math.pi)
+pi = math.pi
+print (pi)
+def CirumferenceofCircle(r):
+	#Solve for Circumferece
+	c = 2 * r * pi
+	print ("Circumference result from function:", C)
+	return c
