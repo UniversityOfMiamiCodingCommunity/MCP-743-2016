234116388844785e3aeaaf7bc5345e3db83f032e
dangerisom <disom@miami.edu>
2017-03-06 13:45:56 -0500

    Dan's comments.

--- assignments-finished/HepatocyteCellLineClass.py
+++ assignments-finished/HepatocyteCellLineClass.py
@@ -1,47 +1,62 @@
 #Hepatocyte cell lines
 
-from os import listdir
-for x in listdir("..\dataFiles"):
-	print(x)
-
-fileInput = open("..\dataFiles\HepatocyteCellLines.txt", "r")
-Cell = ""
-for line in fileInput:
-	result = line.split("\t")
-	print(result)
-	#~ Cell += line[0:-1]
-#~ print(Cell)
-
-#x.split("\t")
+# from os import listdir
+# #for x in listdir("..\dataFiles"):
+# 	print(x)
 
 class Cells:
 
 	def __init__(self):
-		self.name = "Cell Line"
-		self.growth = "Transformed"
-		self.lifespan = "Immortilized"
-		self. response = "Innate Immunity"
-		self.receptor= "HBV Receptor"
+		# This is a class variable for storing the name of the cell line.
+		# This attributed is derived from the column entitled "Cell Line" in the primary source file. 
+		self.nameOfCellLine = ""
+		# This is a class variable for indicating if the cells have been transformed.
+		self.transformed = ""
+		# Leave comment. 
+		self.immortalized = ""
+		# Leave comment.
+		self.immunity = ""
+		# Leave comment.
+		self.receptor= ""
+
+	def set(self, cellLine, transformed, immortalized, immunity, receptor):
+
+		self.nameOfCellLine = cellLine
+		self.transformed = transformed
+		self.immortalized = immortalized
+		self.immunity = immunity
+		self.receptor = receptor
+
 	def printAttributes(self):
 
-		if self.name:
-			print(self.name, "Cell")#want it to report back cell line name: HepRG, HUH7, etc..
-		if self.growth:
-			print("Transformed: ", self.growth) #want it to report back "yes" or "no"
-		if self.lifespan:
-			print("Immortalized: ", self.lifespan)#want it to report back "yes" or "no"
-		if self.response:
-			print("Innate Immunity: ", self.response) #report back "Fully or Moderatley or Slightly Functional"
+		if self.nameOfCellLine:
+			print(self.nameOfCellLine, "Cell")#want it to report back cell line name: HepRG, HUH7, etc..
+		if self.transformed:
+			print("Transformed: ", self.transformed) #want it to report back "yes" or "no"
+		if self.immortalized:
+			print("Immortalized: ", self.immortalized)#want it to report back "yes" or "no"
+		if self.immunity:
+			print("Innate Immunity: ", self.immunity) #report back "Fully or Moderatley or Slightly Functional"
 		if self.receptor:
 			print("HBV Receptor: ", self.receptor) #want it to report back "yes" or "no"
 	
-	# overall 
 
-#~ Hepatocyte = Cell ()
-#~ # Populate the attributes of the class 
-#~ One.name = 
-#~ One.growth = 
-#~ One.lifespan = 
-#~ One.response =
-#~ One.receptor = 
-#~ One.printAttributes()
+fileInput = open("..\dataFiles\HepatocyteCellLines.txt", "r")
+i = 0
+listOfCellInstances = []
+for line in fileInput:
+	if i:
+		result = line.split("\t")
+		#print(result)
+		classInstance = Cells()
+		classInstance.set(result[0], result[1], result[2], result[3], result[4])
+		listOfCellInstances.append(classInstance)
+		#classInstance.printAttributes()
+	i += 1
+
+for x in listOfCellInstances:
+	print x
+
+for x in listOfCellInstances:
+	print "------------------------"
+	x.printAttributes()
