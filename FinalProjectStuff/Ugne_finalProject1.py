aaa45025db63b9bd7f160ba325417536d48695c3
uzekonyte <ugne.zekonyte@med.miami.edu>
2017-03-07 16:07:43 -0500

    final tweak/organization

--- FinalProjectStuff/Ugne_finalProject1.py
+++ FinalProjectStuff/Ugne_finalProject1.py
@@ -1,20 +1,17 @@
-fileInput = open("/Users/ugnzekon/GitHub/MCP-743/FinalProjectStuff/GRIN2A-aligned.fasta.txt", "r")
+
+fileInput = open("/Users/ugnzekon/GitHub/MCP-743/GRIN2A-aligned.fasta.txt", "r")
 import re
-
-######Program that extracts species name from fasta alignment file, 
-######then calculates percentage similarity between the set of protein sequences
-
 
 seq_info = []
 aligned_sequences = []
 species_name_list = []
 for line in fileInput:
 	species_name = []
-	if line[0:3] == ">sp" or line[0:3] == ">tr":   	   #####lines starting with "">sp" and ">tr" have sequence info
+	if line[0:3] == ">sp" or line[0:3] == ">tr":   			#####lines starting with "">sp" and ">tr" have sequence info
 		#print(line)
 		sLine = line.split('OS=')
 		#print(sLine)
-		sLine2 = sLine[1].split(' GN=')                #### LINES NOT SPLIT ON TABS, not all OS values followed by GN= (PE=)
+		sLine2 = sLine[1].split(' GN=')
 		species_name = sLine2[0]
 		#print(sLine2)
 		if " PE=" in sLine2[0]:
@@ -23,15 +20,25 @@ for line in fileInput:
 		species_name_list.append(species_name)
 		#print(species_name)
 		seq_info.append(line)
-	 	#print(seq_info)
+	# 	print(seq_info)
 
-	else:								     ##### all other lines will be sequences
+	# 	os = re.search(' OS=(.+?) (.+?) ', line)  #### LINES NOT SPLIT ON TABS, not all OS values followed by GN= (PE=)
+	# 	if os:
+	# 		species_name.append(os)
+	# 	print(species_name)
+
+	else:								##### all other lines will be sequences
 		aligned_sequences.append(line)
+# 		print(len(line))
+# 		if len(line) != 4103:
+# 			print(line)
+
+# raise SystemExit
 		#print(aligned_sequences)
 
 	# for aligned_sequence in aligned_sequences:
 	# 	print(aligned_sequence)
-	# 	print(len(line))      			     ###############each seq length is 4103
+	# 	print(len(line))      			###############each seq length is 4103
 
 percentage = []
 j = 0 
@@ -40,8 +47,8 @@ for sequence in aligned_sequences:
 	for amino_acid in sequence:
 		if amino_acid == '-':
 			pass
-			#b += 1
-			#percent_dif = 100*(b/len(sequence))
+			#j += 1
+			#percent_dif = 100*(j/len(sequence))
 			#print("Sequence differs by: " + str(percent_dif) + "%")
 		else:
 			i += 1
@@ -50,18 +57,16 @@ for sequence in aligned_sequences:
 	#print(j, len(seq_info), len(aligned_sequences))
 	print("Number of amino acids similar: ", i, "\n", 
 		"Percent sequence similarity: ", percent_sim, "%", "\n", 
-		seq_info[j])
+		species_name, seq_info[j])
 			#print("Sequence similarity: " + str(percent_sim) + "%")
 	j += 1
 		
 
 
 ##make a dictionary that lists species, sequence match (key=species, val=seq%similarity)
-##tupule??
-
+##tupule
 combined = [(species_name_list[i], percentage[i]) for i in range(len(species_name_list))]
-
-#print(combined) ###lists species name, percent similarity
+#print(combined)
 			
 
 fileInput.close()
--- alternate_final.py
+++
@@ -1,7 +0,0 @@
-fileInput = open("/Users/ugnzekon/GitHub/MCP-743/GRIN2A-aligned.fasta.txt", "r")
-import re
-
-
-for line in fileInput:
-	line.split("/t")
-	print(line)
