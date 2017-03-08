f4fdb26cb26ca22c0c5e525f6da16b1cadbc11af
chuan338 <c.chen30@umiami.edu>
2017-03-08 11:28:43 -0500

    20170308 commit

--- assignments-finished/Personal Project_Fission yeast Sts5 protein/Personal Project_Fission yeast Sts5 protein_Chen C.py
+++ assignments-finished/Personal Project_Fission yeast Sts5 protein/Personal Project_Fission yeast Sts5 protein_Chen C.py
@@ -1,60 +1,50 @@
-# Personal Project
+# Personal Project_Identifying putative phosphorylation consensus
 
-# 1) Open the Sts5 protein sequence file in fasta format.
+# This specific project will focus on identifying the putative Orb6 phosphorylation consensus in Sts5 protein sequence.
 
+# 1) 
+# Open the Sts5 protein sequence file in fasta format.
+#########################################################
 fileInput = open("Pombe-sts5.fa") 
-sequence={}
+sequence = {}
 for line in fileInput:
 	if line.startswith(">"):
-		name=line[1:].rstrip("\n")
-		sequence[name]=""
+		name = line[1:].rstrip("\n")
+		sequence[name] = ""
 	else:
 		sequence[name] = sequence[name]+line.rstrip("\n")
-Sts5Protein=sequence[name]
+Sts5Protein = sequence[name]
 print("The Fission yeast S. Pombe Sts5 protein sequence is:",Sts5Protein)
 print("The length of Sts5 protein sequence is:",len(Sts5Protein))
+	
+# 2) 
+# Find all possible Orb6 phosphorylation consensus sequence in parsed Sts5 protein sequence.
+# Write results to file named Orb6Consensus.txt.
+# The file should be formatted such that:
+# a) the first line reports the total number of the putative Orb6 phosphorylation consensus. 
+# b) the additional lines report a single Orb6 phosphorylation site index
+############################################################################################
+## Load a module using the import statement: module dealing with regular expression (re)
+#import re
+# Define the pattern I would like to search for
+pattern = "H.(R|K|H)..(S|T)"
+## Use the regular search function
+#re.search (pattern, Sts5Protein)
+#St5Protein = sequence[name]
+#if re.search(r"H.(R|K|H)..(S|T)", Sts5Protein):
+	#print("The Orb6 phosphorylation consensus in Sts5 protein is found")
 
-## 2) Still on going
+patternCount = 0
+i = 0
+Orb6Consensus = open("C:/Users/Samuel/Documents/GitHub/MCP-743/assignments-finished/Personal Project_Fission yeast Sts5 protein/Orb6Consensus.txt", "w")
+patternCount = Sts5Protein.count(pattern)
+Orb6Consensus.write("The total number of Orb6 phosphorylation consensus found in the Sts5 protein sequence is: "+str(patternCount)+"\n")
+while i < len(Sts5Protein) - 5:
+	iPattern = Sts5Protein[i:i+6]
+	if iPattern == pattern:
+		Orb6Consensus.write("Found Orb6 consensus "+ iPattern+ " at index "+str(i+6)+"\n") 
+	i += 1
+Orb6Consensus.close()
 
-## Find all possible Orb6 phosphorylation consensus sequence in Sts5 protein.
-## Find all possible start codons in parsed DNA sequence.
-## Write your results to file named startCodonIndices.txt.
-## The file should be formatted such that:
-## a) the first line reports the total number of startCodons
-## b) the additional lines report a single start codon index
-##################################################################################################
-#startCodon = "ATG"
-#stopCodons = ["TAG", "TGA", "TAA"]
 
-#startCodonCount = 0
-#i = 0
-#startCodonIndices = open("C:/Users/Samuel/Documents/GitHub/MCP-743/startCodonIndices.txt","w")
-#startCodonCount = dna.count(startCodon)
-#startCodonIndices.write("The total number of start codons found in the p53 DNA sequence is: "+str(startCodonCount)+"\n")
-#while i < len(dna) - 2:
-	#iCodon = dna[i:i+3]
-	#if iCodon == startCodon:
-		#startCodonIndices.write("Found start codon "+ iCodon+ " at index "+str(i)+"\n") 
-	#i += 1
-#startCodonIndices.close()
 
-## 3)
-## Find all possible stop codons in parsed DNA sequence.
-## Write your results to file named stopCodonIndices.txt.
-## The file should be formatted such that:
-## a) the first line reports the total number of stopCodons
-## b) the additional lines report a single stop codon index 
-##################################################################################################
-#stopCodonIndices = open("C:/Users/Samuel/Documents/GitHub/MCP-743/stopCodonIndices.txt","w")
-#stopCodonCount = 0
-#for stopCodon in stopCodons:
-	#stopCodonCount = stopCodonCount+dna.count(stopCodon)
-#stopCodonIndices.write("The total number of stop codons found in the p53 DNA sequence is: "+str(stopCodonCount)+"\n")
-#for stopCodon in stopCodons:
-	#j = 0
-	#while j < len(dna) - 2:
-		#jCodon = dna[j:j+3]
-		#if jCodon == stopCodon:
-			#stopCodonIndices.write("Found stop codon "+ jCodon+ " at index "+str(j)+"\n") 
-		#j += 1
-#stopCodonIndices.close()
